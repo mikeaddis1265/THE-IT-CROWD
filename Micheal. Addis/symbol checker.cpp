f77638d1f00bc709e3b940396ca7b1423e4eeaa5
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-03-14 03:18:00 -0400

    Create symbol checker.cpp

---
+++ Micheal. Addis/symbol checker.cpp
@@ -0,0 +1,31 @@
+include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"enter symbol:";
+char a;
+cin>>a;
+
+if(a>='A' && a<='Z')
+  
+    if(a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+    cout<<"is upper voule";
+    else
+    cout<<"upper consolant";  
+
+else if(a>='a' && a<='z')
+    if(a=='a' || a=='e' || a=='e' || a=='o' || a=='u')
+    cout<<"is lower vouble";
+    else
+    cout<<"is lower consolant";
+
+else if(a<='9' && a>='0')
+     if(a%2==2)
+     cout<<"even";
+else
+     cout<<"odd";     
+    
+else
+ cout<<"panctuation";     
+}
